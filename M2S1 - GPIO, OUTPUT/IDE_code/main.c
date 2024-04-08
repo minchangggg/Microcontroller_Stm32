1b6dc954cfb67c92dcf22a46dc2a618722019c12
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-04-08 17:01:16 +0700

    Update main.c

--- M2S1 - GPIO, OUTPUT/IDE_code/main.c
+++ M2S1 - GPIO, OUTPUT/IDE_code/main.c
@@ -98,7 +98,7 @@ int main(void)
 //	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 //	HAL_Delay(100);
 //	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-//  HAL_Delay(100);
+//  	HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
